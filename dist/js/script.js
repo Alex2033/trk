7e1e8dd23af476e478180e4fb429e880760195ff
Александр <XoxlofAlexandr@yandex.ru>
2018-10-30 23:39:09 +0300

    1) В секции арендаторов на главной странице стрелочки сделаны по центру слайдера 2) В секции арендаоров на главной странице отступ от заголовка к слайдеру уменьшен 3) В секции акций и арендаторов на главной странице заголовку добавлен отступ 4) В секции акций и арендаторов на главной странице заголовок сделан ссылкой (ИЗМЕНЕНА СТРУКТУРА) 5) На странице site-map.html устранен баг появления полей 6) На странице site-map.html на мобильных устройствах добавлен отступ контента справа и слева 7) На странице card.html заголовок выровнен по надписи Магазины в меню (ИЗМЕНЕНА СТРУКТУРА) 8) На странице contact.html у факсов и адреса убраны ссылки (ИЗМЕНЕНА СТРУКТУРА) 9) На странице shops.html заголовок Магазины выровнен по конетнту на десктопе 10) На странице shops.html расстояние между табами и категорями уменьшено в два раза 11) На странице shop-zona.html расстояние между табами и картинка уменьшено в два раза 12) На мобильнике в футере адреси номер телефона выровнены по левому краю 13) Уменьшено расстояние в хедере между верхней строкой и нижней на мобильном разрешении 14) Расстояние между строками в хедере на планшете сделано равным

--- dist/js/script.js
+++ dist/js/script.js
@@ -359,3 +359,16 @@ $(document).mouseup(function(e) {
         });
     }
 });
+
+$(document).ready(function() {
+    $('#form').submit(function() {
+        $.ajax({
+            type: 'POST',
+            url: 'mail.php',
+            data: $(this).serialize()
+        }).done(function() {
+            alert("Ваше сообщение отправлено!");
+        });
+        return false;
+    });
+});
